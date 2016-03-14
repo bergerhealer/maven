3f60b9bb6a03dd0ba0b8cd403d8a9d92bb8eede1
Matthijs <matthijs.giezenberg@gmail.com>
2016-03-14 21:34:38 +0100

    Update TC

--- scripts/publish-tc.bat
+++ scripts/publish-tc.bat
@@ -1,4 +1,4 @@
 @ECHO off
 @ECHO TrainCarts
 
-mvn install:install-file -DgroupId=com.bergerkiller.bukkit -DartifactId=TrainCarts -Dversion=1.75-SNAPSHOT -Dpackaging=jar -Dfile=C:\Users\Develop\Documents\GitHub\TrainCarts\target\TrainCarts.jar -DlocalRepositoryPath=C:\Users\Develop\Documents\GitHub\maven
+mvn install:install-file -DgroupId=com.bergerkiller.bukkit -DartifactId=TrainCarts -Dversion=1.76-SNAPSHOT -Dpackaging=jar -Dfile=C:\Users\Develop\Documents\GitHub\TrainCarts\target\TrainCarts.jar -DlocalRepositoryPath=C:\Users\Develop\Documents\GitHub\maven
