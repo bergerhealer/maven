d8fda565f670cc4968268a001247e11dc5a72c5b
Matthijs <matthijs.giezenberg@gmail.com>
2016-03-14 20:43:29 +0100

    Update BKC

--- scripts/publish-bk.bat
+++ scripts/publish-bk.bat
@@ -1,4 +1,4 @@
 @ECHO off
 @ECHO BKCommonLib
 
-mvn install:install-file -DgroupId=com.bergerkiller.bukkit -DartifactId=BKCommonLib -Dversion=1.66-SNAPSHOT -Dpackaging=jar -Dfile=C:\Users\Develop\Documents\GitHub\BKCommonLib\target -DlocalRepositoryPath=C:\Users\Develop\Documents\GitHub\maven
+mvn install:install-file -DgroupId=com.bergerkiller.bukkit -DartifactId=BKCommonLib -Dversion=1.70-SNAPSHOT -Dpackaging=jar -Dfile=C:\Users\Develop\Documents\GitHub\BKCommonLib\target\BKCommonLib.jar -DlocalRepositoryPath=C:\Users\Develop\Documents\GitHub\maven
